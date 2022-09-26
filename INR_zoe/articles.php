164d522490433867538b8c43fb03c5fdc6af24ab
zoeleroux <zoe.leroux21@gmail.com>
2022-09-26 16:02:18 +0200

    INR

---
+++ INR_zoe/articles.php
@@ -0,0 +1,115 @@
+<!-- 
+  &#160; = espace insécable (empèche le passage à la ligne)
+
+  &#8201; = espace fine (avec la ponctuation double (:, !, ?,""))
+
+  tous les codes -> https://www.la-grange.net/w3c/html4.01/sgml/entities.html#h-24.4
+
+-->
+
+
+<section>
+      
+  <aside>    
+    <h2>
+      Zoé Leroux 
+      <a href="" target="_blank">portfolio</a>
+    </h2>
+    <p>
+      le conte
+    </p>
+  </aside>
+
+
+<!-- 
+     _         _   _      _      
+    / \   _ __| |_(_) ___| | ___ 
+   / _ \ | '__| __| |/ __| |/ _ \
+  / ___ \| |  | |_| | (__| |  __/
+ /_/   \_\_|   \__|_|\___|_|\___|
+ -->
+
+  <article class="filterDiv image">
+    <h3>
+      MELOT Michel.  
+    </h3>
+
+    <h4>
+      <a href="https://leseditionsdu81.fr/livres/une-breve-histoire-de-lecriture-michel-melot/" target="_blank">
+        Une brève Histoire de l'Image, 
+      </a>
+    </h4> 
+    
+    <h5>
+      Les éditions du 81, 2015.
+    </h5>
+    
+    <blockquote cit="#">L'image est à la fois <em>accès</em> à une réalité absente qu'elle évoque symboliquement, et <em>obstacle</em> à cette réalité. Double sens du mot écran : transparence et opacité</blockquote>
+    
+    <p> 
+      Revenant sur l'Histoire de l'image, des grottes aux écrans numériques, des icônes religieuses au croquis scientifique, l'auteur s'attache à trouver une définition complète et satisfaisante de l'image. Parant du postulat selon lequel l'image n'est pas la ressemblance, Michel Melot explique leur puissance par les modèles antérieurs, les codes et les mondes intérieurs auxquels elles font référence. 
+    </p>
+   
+
+    <h6> 
+      lu en juin 2022 
+    </h6>
+  
+  </article>
+
+
+<!-- 
+     _         _   _      _      
+    / \   _ __| |_(_) ___| | ___ 
+   / _ \ | '__| __| |/ __| |/ _ \
+  / ___ \| |  | |_| | (__| |  __/
+ /_/   \_\_|   \__|_|\___|_|\___|
+ -->
+
+  <article class="filterDiv narration">
+    <h3>
+      SEDDIKI, Sabrina.  
+    </h3>
+
+    <h4>
+      <a href="https://www.arteradio.com/son/61664900/comptines_cruelles" target="_blank">
+        Comptines cruelles, 
+      </a>
+    </h4> 
+    
+    <h5>
+      Produit par ARTE Radio, réalisé par Samuel Hirsch, publié le 15/12/2020.
+    </h5>
+    
+    <blockquote cit="#">Cette comptine nous dit que le monde de l'enfance n'est pas si doux. Heureusement, la naiveté enfantine, la candeur de l'enfant, y voit l'émerveillement de la magie.</blockquote>
+    
+    <p> 
+      Du sexe et de la torture. Derrière les histoires irrationnelles des comptines, comme c'est aussi le cas pour les contes de notre enfance, se cache souvent une réalité bien plus féroce qu'il n'y paraît. En prenant pour exemple "Au clair de la Lune", "Une souris verte" ou encore "Gentil Coquelicot", Marie-Claire Bruley, psychologue, souligne dans ce podcast court que ces mélodies entêtantes ont toujours été destinées aux adultes et en expose les origines.
+    </p>
+   
+
+    <h6> 
+      écouté le 15/09/2022  
+    </h6>
+  
+  </article>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</section>
+
+
+
+
---
+++ INR_zoe/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="UTF-8">
+    <title>Références</title>
+  </head>
+
+  <body>
+    
+    <?php 
+      require "articles.php"; 
+    ?>
+  
+  </body>
+
+</html>
