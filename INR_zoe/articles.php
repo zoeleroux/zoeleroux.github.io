ea8301fbc6bbb7ae990a5163f23a5d73c82d8d0a
zoeleroux <zoe.leroux21@gmail.com>
2022-10-10 16:00:11 +0200

    Update articles.php

--- INR_zoe/articles.php
+++ INR_zoe/articles.php
@@ -3,7 +3,11 @@
 
   &#8201; = espace fine (avec la ponctuation double (:, !, ?,""))
 
+  &#8239; = espace fine insécable (empèche le passage à la ligne)
+
   tous les codes -> https://www.la-grange.net/w3c/html4.01/sgml/entities.html#h-24.4
+
+
 
 -->
 
@@ -12,11 +16,11 @@
       
   <aside>    
     <h2>
-      Zoé Leroux 
+      Zoé, Leroux 
       <a href="" target="_blank">portfolio</a>
     </h2>
     <p>
-      le conte
+      Les messages cachés dans le conte
     </p>
   </aside>
 
@@ -31,7 +35,7 @@
 
   <article class="filterDiv image">
     <h3>
-      MELOT Michel.  
+      MELOT, Michel.  
     </h3>
 
     <h4>
@@ -44,7 +48,7 @@
       Les éditions du 81, 2015.
     </h5>
     
-    <blockquote cit="#">L'image est à la fois <em>accès</em> à une réalité absente qu'elle évoque symboliquement, et <em>obstacle</em> à cette réalité. Double sens du mot écran : transparence et opacité</blockquote>
+    <blockquote cit="#">L'image est à la fois <em>accès</em> à une réalité absente qu'elle évoque symboliquement, et <em>obstacle</em> à cette réalité. Double sens du mot écran&#8239;: transparence et opacité</blockquote>
     
     <p> 
       Revenant sur l'Histoire de l'image, des grottes aux écrans numériques, des icônes religieuses au croquis scientifique, l'auteur s'attache à trouver une définition complète et satisfaisante de l'image. Parant du postulat selon lequel l'image n'est pas la ressemblance, Michel Melot explique leur puissance par les modèles antérieurs, les codes et les mondes intérieurs auxquels elles font référence. 
@@ -93,6 +97,89 @@
     </h6>
   
   </article>
+
+
+
+
+
+<!-- 
+     _         _   _      _      
+    / \   _ __| |_(_) ___| | ___ 
+   / _ \ | '__| __| |/ __| |/ _ \
+  / ___ \| |  | |_| | (__| |  __/
+ /_/   \_\_|   \__|_|\___|_|\___|
+ -->
+
+  <article class="filterDiv typographie interactif">
+    <h3>
+      BOIVIN, Diane.  
+    </h3>
+
+    <h4>
+      <a href="https://etapes.com/diane-boivin-evoque-le-concept-de-lecture-contrariee/" target="_blank">
+        Diane Boivin évoque le concept de lecture contrariée, 
+      </a>
+    </h4> 
+    
+    <h5>
+      étapes:, 2012.
+    </h5>
+    
+    <blockquote cit="https://cargocollective.com/dianeboivin/lecture-relecture">Ma pratique du graphisme s’oriente vers cette notion de l’OULEPO – ouvroir de lectures potentielles – en référence au mouvement littéraire OULIPO.</blockquote>
+    
+    <p> À l’occasion d’une réédition de Blanche Neige, Diane Boivin se saisit de sa typographie <em>oulepo</em> qui repose ce que l’auteur appelle la lecture contrariée. Ici, les caractères se renversent, s’imbriquent et se retournent, jouant sur l’ambiguïté entre lettre et dessin. L’information est en quelque sorte scénarisée puisque face à cette écriture codée, le lecteur cherche activement les outils pour déchiffrer cette image-texte. Ainsi, le texte en bleu est celui du Blanche Neige, le conte insouciant de la mémoire collective&#8239;; auquel est superposé en rouge l’analyse crue du conte par le psychologue Marc Girard. Si les textes apparaissent de première abord comme illisibles, des calques bleu et rouge permettent de les révéler et de les lires l’un après l’autre.
+      
+    </p>
+   
+
+    <h6> 
+      consulté le 04/10/2022 
+    </h6>
+  
+  </article>
+
+
+
+
+
+<!-- 
+     _         _   _      _      
+    / \   _ __| |_(_) ___| | ___ 
+   / _ \ | '__| __| |/ __| |/ _ \
+  / ___ \| |  | |_| | (__| |  __/
+ /_/   \_\_|   \__|_|\___|_|\___|
+ -->
+  
+   <article class="filterDiv imprime art">
+    <h3>
+      MOREL, Claire.  
+    </h3>
+
+    <h4>
+      <a href="https://actualitte.com/article/19860/interviews/claire-morel-l-artiste-qui-murmurait-a-l-oreille-des-livres" target="_blank">
+        Claire Morel, l'artiste qui murmurait à l'oreille des livres, 
+      </a>
+    </h4> 
+    
+    <h5>
+      Laure Besnier, 03/05/2018 à 11:06.
+    </h5>
+    
+    <blockquote cit="http://www.metaproject.net/fr/articles/author/152/claire-morel">Je cherchais donc une pièce à faire autour du manuscrit. Et je me suis interrogée : est-ce que dessiner des lettres était un geste plus proche du dessin ou de l’écrit ?</blockquote>
+    
+    <p> 
+      Lorsque le livre est traité non pas pour son contenu mais pour sa matérialité même, est-il encore un livre ? En utilisant le livre comme matière première dans sa création, Claire Morel floute la frontière entre édition et arts plastiques. Plus qu’un produit d’une technologie, elle dit considérer le livre comme un paysage, un espace, une architecture qui se déploie et se dilate. Travaillant sur l’absence, sur ce qui est oublié, Claire Morel laisse souvent de côté l’écrit de l’ouvrage pour se concentrer sur ce qu’est l’essence du livre&#8239;: papier et encre. 
+    </p>
+   
+
+    <h6> 
+      consulté le 05/10/2022 
+    </h6>
+  
+  </article>
+
+
+
 
 
 
